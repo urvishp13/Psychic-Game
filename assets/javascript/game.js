5f9a4719847ed375515f093a50acd096e946ce55
Picasso <urvishp13@gmail.com>
2022-05-05 16:47:36 -0400

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+var database = "abcdefghijklmnopqustuvwxyz"; /* all the lowercase letters */
+
+/* Get the letter to guess for this game */
+var letterForGame = database.charAt(Math.floor(Math.random() * 26));
+
+/* Going to be used to add guessed letters to this area */
+var lettersGuessedArea = document.getElementById("letters-guessed");
+
+var numberOfGuesses = document.getElementById("guesses-left").innerHTML;
+while (numberOfGuesses > 0) {
+    let letterGuessed = "";
+    Document.addEventListener('keydown', (letterPressed) => {
+        console.log("letter pressed: " + letterPressed);
+        letterGuessed = letterPressed;
+    });
+    // If letter guessed is the correct letter
+    if (letterGuessed === letterForGame) {
+        // YOU WON!
+        document.getElementById("wins").innerHTML++;
+        break;
+    }
+    // If letter guessed is incorrect
+    else {
+        // Add letter guessed to letter-guessed-area
+        lettersGuessedArea.textContent += letterGuessed;
+        // Decrement guesses avaiable by 1
+        numberOfGuesses--
+        // Update guesses left in DOM
+        document.getElementById("guesses-left").innerHTML--;
+    }
+}
+document.getElementById('losses').innerHTML++;
