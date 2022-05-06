9427a1ffedfd796d1a4b5de282ec6aeddc023792
Picasso <urvishp13@gmail.com>
2022-05-06 17:32:46 -0400

    moved if-statement inside event listener

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,25 +8,23 @@ var lettersGuessedArea = document.getElementById("letters-guessed");
 
 var numberOfGuesses = document.getElementById("guesses-left").innerHTML;
 while (numberOfGuesses > 0) {
-    let letterGuessed = "";
-    Document.addEventListener('keydown', (letterPressed) => {
+    document.addEventListener('keydown', (letterPressed) => {
         console.log("letter pressed: " + letterPressed);
-        letterGuessed = letterPressed;
+        var letterGuessed = letterPressed;
+        // If letter guessed is the correct letter
+        if (letterGuessed === letterForGame) {
+            // YOU WON!
+            document.getElementById("wins").innerHTML++;
+        }
+        // If letter guessed is incorrect
+        else {
+            // Add letter guessed to letter-guessed-area
+            lettersGuessedArea.innerHTML += letterGuessed + ", ";
+            // Decrement guesses avaiable by 1
+            numberOfGuesses--
+            // Update guesses left in DOM
+            document.getElementById("guesses-left").innerHTML--;
+        }
     });
-    // If letter guessed is the correct letter
-    if (letterGuessed === letterForGame) {
-        // YOU WON!
-        document.getElementById("wins").innerHTML++;
-        break;
-    }
-    // If letter guessed is incorrect
-    else {
-        // Add letter guessed to letter-guessed-area
-        lettersGuessedArea.innerHTML += letterGuessed + ", ";
-        // Decrement guesses avaiable by 1
-        numberOfGuesses--
-        // Update guesses left in DOM
-        document.getElementById("guesses-left").innerHTML--;
-    }
 }
 document.getElementById('losses').innerHTML++;
